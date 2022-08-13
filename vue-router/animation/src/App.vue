622f27f6e9df7263c7108ae864aeac23ac84c1bd
河图洛书 <230435951@qq.com>
2022-08-13 19:38:28 +0800

    create animation

---
+++ vue-router/animation/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+  <nav>
+    <router-link :to="{ name: 'home' }">home</router-link>
+    <router-link :to="{ name: 'about' }">about</router-link>
+  </nav>
+  <main>
+    <router-view #default="{ Component }">
+      <transition name="fade">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+  </main>
+
+</template>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+
+  a {
+    padding: 10px 20px;
+    background-color: rgb(30, 100, 77);
+    color: #fff;
+    margin-right: 10px;
+  }
+}
+
+main {
+  margin-top: 20px;
+  position: relative;
+}
+
+.fade-enter-from, .fade-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: all 1s;
+}
+.fade-enter-to, .fade-leave-from {
+  transform: translateX(0);
+}
+</style>
---
+++ vue-router/animation/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae, eveniet rerum, eos ipsa adipisci blanditiis sit
+      ducimus voluptatum dolorem corrupti repudiandae, eligendi aperiam magni neque deserunt aspernatur? Non eaque culpa
+      facilis consectetur et similique nobis, enim in, doloremque nostrum excepturi commodi molestias repellendus.
+      Inventore ab soluta sed maiores ipsum harum, mollitia impedit omnis quidem porro sequi hic at alias eius voluptatem
+      deserunt, natus consectetur aut quisquam earum dolorum aperiam nulla esse? Earum harum natus, vitae, dolores officia
+      expedita blanditiis reiciendis voluptates neque voluptatem fugiat ipsam praesentium. Sint deleniti cupiditate
+      laborum modi iusto beatae quibusdam molestiae, a dolores consectetur assumenda.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  position: absolute;
+
+  p {
+    margin-top: 20px;
+    line-height: 1.7;
+  }
+}
+</style>
